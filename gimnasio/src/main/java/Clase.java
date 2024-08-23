62a4eaa09f72d0e9721517ef3abefebcf37de8ad
Shaampuu <161147293+Shaampuu@users.noreply.github.com>
2024-08-22 21:19:53 -0500

    .

--- gimnasio/src/main/java/Clase.java
+++ gimnasio/src/main/java/Clase.java
@@ -1,6 +1,6 @@
 import enums.EstadoClase;
 import enums.TipoClase;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.*;
 
 @AllArgsConstructor
@@ -14,14 +14,43 @@ public class Clase {
     // Atributos
     private String Id;
     private String nombre;
-    private String horario; //List<String>
+    private LocalDateTime horario; //List<String>
     private int capacidad;
-    private LocalDate fechaInicio;
-    private LocalDate fechaFin;
-    private EstadoClase estado;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+    private boolean estado;
     private TipoClase tipoClase;
     private Entrenador entrenador;
     //disponible boolean
     //inscritos
 
+    public Clase(String id, String nombre, LocalDateTime horario, int capacidad,LocalDateTime fechaInicio, LocalDateTime fechaFin, boolean estado, TipoClase TIPOCLASE, Entrenador ENTRENADOR){
+        this.Id = id;
+        this.nombre = nombre;
+        this.horario = horario;
+        this.capacidad = capacidad;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.estado = estado;
+        this.tipoClase = TIPOCLASE;
+        this.entrenador = ENTRENADOR;
+    }
+
+    public boolean reservarClase(){
+        if(capacidad > 0){
+            capacidad--;
+            return true;
+        }
+        return false;
+    }
+
+    public void cancalarReserva(){
+        capacidad++;
+    }
+
+    public String getId() {
+        return Id;
+    }
+    public void setId(String id) {}
+    
 }
