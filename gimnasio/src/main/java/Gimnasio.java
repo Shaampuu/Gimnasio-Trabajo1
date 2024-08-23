32027c7601c3e7771877e7c2f232b510504516ea
Viverosj <141973610+Viverosj@users.noreply.github.com>
2024-08-22 21:28:01 -0500

    .

--- gimnasio/src/main/java/Gimnasio.java
+++ gimnasio/src/main/java/Gimnasio.java
@@ -20,14 +20,14 @@ public class Gimnasio {
         this.entrenadores = entrenadores;
     }
 
-    //metodo de agregar usuario
+    //metodo de agregar usuario //angelica
     //metodo de eliminar usuario
     //metodo de actualizar usuario
-    //metodo de busqueda de clases
+    //metodo de busqueda de clases //juanita
     //metodo de reserva de clases
     //metodo de cancelacion de reserva de clases
-    //metodo de registro de entrenamientos
-    //metodo de consulta de historial de entrenamientos
+    //metodo de registro de entrenamientos //simon
+    //metodo de consulta de historial de entrenamientos 
     //metodo de generacion de reportes
     // metodo de consulta de disponibilidad de una clase
 }
